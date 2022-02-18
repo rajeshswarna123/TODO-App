f1cb70627e22219810379390b078c989b98b77c5
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-02-18 14:44:09 -0500

    Added express in index.js

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,24 @@
-const http = require('http');
+const express = require('express')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
-server.listen(port, hostname, () =>{
-    console.log(`Server running at http://${hostname}:${port}`);
-});
+app
+    .get('/', (req, res) => {
+        res.send('This is TODO app and You are on the homepage');
+    })
+    .get('/about', (req, res) => {
+        res.send('This is TODO app and You are on the about page');
+    })
+    .get('/contact', (req, res) => {
+        res.send({
+            email: 'swarnar1@newpaltz.edu',
+            phone:'845-750-8121',
+            twitter: 'notwitter',   
+            instagram: 'rajesh_swarna'
+        });
+      })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
