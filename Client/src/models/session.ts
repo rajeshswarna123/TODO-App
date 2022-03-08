8b3d6a5f05a312e7de0469838305c5d4db9a7a54
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-03-08 18:12:08 -0500

    Added user and session models

---
+++ Client/src/models/session.ts
@@ -0,0 +1,28 @@
+import { reactive } from "vue";
+
+import router from "../router";
+import * as users from "../models/user";
+
+
+const session = reactive({
+    user: null as users.User | null,
+})
+
+export async function Login(handle: string, password: string) {
+    const user = users.list.find(u => u.handle === handle);
+
+    if (!user) {
+        throw { message: "User not found" };
+    }
+    if(user.password !== password) {
+        throw { message: "Incorrect password" };
+    }
+
+    session.user = user;
+    router.push('/messages');
+}
+
+export function Logout() {
+    session.user = null;
+}
+export default session;
---
+++ Client/src/models/user.ts
@@ -0,0 +1,40 @@
+
+export interface User {
+    firstName: string;
+    lastName: string;
+    handle: string;
+    password: string;
+    email: string;
+    pic: string;
+    id: number;
+}
+
+export const list: User[] = [
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        handle: 'raj',
+        password: 'password',
+        email: 'jhon@doe.com',
+        pic: 'https://randomuser.me/api/portraits/men/1.jpg',
+        id: 1,
+    },
+    {
+        firstName: 'Vladimir',
+        lastName: 'Putin',
+        handle: 'sunny',
+        password: 'long table',
+        email: 'jhon@doe.com',
+        pic: 'https://randomuser.me/api/portraits/men/2.jpg',
+        id: 2,
+    },
+    {
+        firstName: 'Kamala',
+        lastName: 'Harris',
+        handle: 'vp',
+        password: 'password',
+        email: 'kamala@whitehouse.org',
+        pic: 'https://randomuser.me/api/portraits/women/3.jpg',
+        id: 3,
+    },
+];
