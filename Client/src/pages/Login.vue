755c2fbdb4ec68572f41e18fc02287c049edc884
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-03-08 19:01:24 -0500

    Added inputs in Login page

--- Client/src/pages/Login.vue
+++ Client/src/pages/Login.vue
@@ -1,12 +1,44 @@
+<script setup lang="ts">
+import { Login } from "../models/session"
+
+
+function badLogin(){
+    Login("Rajesh Sunny", "meme")
+}
+
+function login()
+{
+    Login(username.value, password.value)
+}
+</script>
+
 <template>
-    <div>
-        Login Page !
+    <div class="section">
+         <h1 class="title">Login</h1>
+        <div>
+            <label for="username">User Name</label>
+            <input id="username" class="input" placeholder="User Name " v-model="username" />
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input id="password" class="input" placeholder="Password" v-model="password" />
+        </div>
+        <button class="button is-primary my-5" @click="login"> 
+            <span class="icon">
+               <i class="fa fa-sign-in"></i>
+            </span>
+            <span>Login</span>
+        </button>
+        &nbsp;
+        <router-link class="button is-warning my-5" to="/signup">
+            <span class="icon">
+                <i class="fa fa-sign-in"></i>
+            </span>
+            <span>Signup</span>
+        </router-link>
     </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
 
 <style scoped>
 
