36dbeeac78a69d51b38bbf9b83210f37829a10ea
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-03-10 21:12:44 -0500

    changed login page and removed unwanted code in home page

--- Client/src/pages/Home.vue
+++ Client/src/pages/Home.vue
@@ -33,33 +33,6 @@ function submitForm(e){
        <div class="section">
        <div class="container">
         <div class="columns">
-          <div class="column is-3">
-            <div class="card">
-              <header class="card-header">
-                <p class="card-header-title">
-                  Component
-                </p>
-                <button class="card-header-icon" aria-label="more options">
-                  <span class="icon">
-                    <i class="fas fa-angle-down" aria-hidden="true"></i>
-                  </span>
-                </button>
-              </header>
-              <div class="card-content">
-                <div class="content">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                  <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
-                  <br>
-                  <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                </div>
-              </div>
-              <footer class="card-footer">
-                <a href="#" class="card-footer-item">Save</a>
-                <a href="#" class="card-footer-item">Edit</a>
-                <a href="#" class="card-footer-item">Delete</a>
-              </footer>
-            </div>
-          </div>
           <div class="column">
               <article class="panel">
                 <div class="tabs is-boxed">
@@ -91,22 +64,25 @@ function submitForm(e){
                   </ul>
                 </div>
                 <div class="panel-block">
-                  <form @submit.prevent="submitForm">
-                  <p class="control has-icons-left">
+                  <form @submit.prevent="submitForm" class="columns">
+                  <div class="control has-icons-left column">
                     <input class="input is-primary" type="text" placeholder="New Task" v-model="newTask">
                     {{newTask}}
                     <span class="icon is-left">
                       <i class="fas fa-calendar-plus" aria-hidden="true"></i>
                     </span>
-                  </p>
+                  </div>
                   <div>
                     <input type="date" class="input" v-model="dueDate"/>
-                     <select v-model="assignedTo">
+                     
+                  </div>
+                  <div class="column">
+                    <select v-model="assignedTo" class="select">
                       <option disabled selected>Assign to</option>
                       <option v-for="user in users.list" :value="user.id">{{user.handle}}</option>
                     </select>
-                    <button  type="submit" class="button">Create</button>
                   </div>
+                    <button  type="submit" class="button">Create</button>
                   </form>
                 </div>
                 <a class="panel-block columns" v-for="(task,i) in allTasks.tasks" :class="{'text-dec-line-through' : task.isCompleted==true}" v-show="(((currentTab=='All') || (currentTab=='Upcoming') || ((currentTab=='Current') && (!task.isCompleted)) || ((currentTab=='Completed') && task.isCompleted)) && ((task.isOwned == session.user.id) || (task.assignedTo == session.user.id)))">
@@ -125,44 +101,6 @@ function submitForm(e){
                     </div>
                 </a>
               </article>
-          </div>
-          <div class="column is-3">
-            <h1 class="title">Table of contents</h1>
-            <h2 class="subtitle">Intra-page navigation</h2>
-            <aside class="menu">
-              <p class="menu-label">
-                General
-              </p>
-              <ul class="menu-list">
-                <li><a>Dashboard</a></li>
-                <li><a>Customers</a></li>
-              </ul>
-              <p class="menu-label">
-                Administration
-              </p>
-              <ul class="menu-list">
-                <li><a>Team Settings</a></li>
-                <li>
-                  <a class="is-active">Manage Your Team</a>
-                  <ul>
-                    <li><a>Members</a></li>
-                    <li><a>Plugins</a></li>
-                    <li><a>Add a member</a></li>
-                  </ul>
-                </li>
-                <li><a>Invitations</a></li>
-                <li><a>Cloud Storage Environment Settings</a></li>
-                <li><a>Authentication</a></li>
-              </ul>
-              <p class="menu-label">
-                Transactions
-              </p>
-              <ul class="menu-list">
-                <li><a>Payments</a></li>
-                <li><a>Transfers</a></li>
-                <li><a>Balance</a></li>
-              </ul>
-            </aside>
           </div>
         </div>
       </div>
--- Client/src/pages/Login.vue
+++ Client/src/pages/Login.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { ref } from "vue-demi"
 import { Login } from "../models/session"
 
-
+const username= ref();
+const password= ref();
 function badLogin(){
     Login("Rajesh Sunny", "meme")
 }
@@ -14,29 +16,45 @@ function login()
 
 <template>
     <div class="section">
-         <h1 class="title">Login</h1>
-        <div>
-            <label for="username">User Name</label>
-            <input id="username" class="input" placeholder="User Name " v-model="username" />
+
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-black">Login</h3>
+                    <hr class="login-hr">
+                    <p class="subtitle has-text-black">Please login to proceed.</p>
+                    <div class="box">
+                        <form>
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-large" placeholder="Your Email" autofocus=""  v-model="username" />
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-large" type="password" placeholder="Your Password" v-model="password" />
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="checkbox">
+                  <input type="checkbox">
+                  Remember me
+                </label>
+                            </div>
+                            <button class="button is-block is-info is-large is-fullwidth"  @click="login">Login <i class="fa fa-sign-in" aria-hidden="true"></i></button>
+                        </form>
+                    </div>
+                    <p class="has-text-grey">
+                        <router-link to="/signup">Sign Up</router-link> &nbsp;·&nbsp;
+                        <a href="../">Forgot Password</a> &nbsp;·&nbsp;
+                        <a href="../">Need Help?</a>
+                    </p>
+                </div>
+            </div>
         </div>
-        <div>
-            <label for="password">Password</label>
-            <input id="password" class="input" placeholder="Password" v-model="password" />
-        </div>
-        <button class="button is-primary my-5" @click="login"> 
-            <span class="icon">
-               <i class="fa fa-sign-in"></i>
-            </span>
-            <span>Login</span>
-        </button>
-        &nbsp;
-        <router-link class="button is-warning my-5" to="/signup">
-            <span class="icon">
-                <i class="fa fa-sign-in"></i>
-            </span>
-            <span>Signup</span>
-        </router-link>
     </div>
+
 </template>
 
 
