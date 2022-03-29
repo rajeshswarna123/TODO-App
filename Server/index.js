fcb07f7dd9a85931985b505164ca54cceaebf4c8
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-03-29 15:51:58 -0400

    updated index.js file

--- Server/index.js
+++ Server/index.js
@@ -1,23 +1,12 @@
-const express = require('express')
+const express = require('express');
 
 const app = express()
 const port = 3000
 
 app
-    .get('/', (req, res) => {
-        res.send('This is TODO app and You are on the homepage');
+    .get('/api/', (req, res) => {
+        res.send('You are on the homepage');
     })
-    .get('/about', (req, res) => {
-        res.send('This is TODO app and You are on the about page');
-    })
-    .get('/contact', (req, res) => {
-        res.send({
-            email: 'swarnar1@newpaltz.edu',
-            phone:'845-750-8121',
-            twitter: 'notwitter',   
-            instagram: 'rajesh_swarna'
-        });
-      })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
