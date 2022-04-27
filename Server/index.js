1aa4bec0e13d8147484b215cc769dbaa3adf5bbf
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-04-27 09:53:09 -0400

    Made changes in Index.js file

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,54 @@
-const express = require('express');
+require('dotenv').config();
+const express = require('express')
+const res = require('express/lib/response')
 
-const usersController = require('./controllers/users');
+const usersController = require('./controllers/users')
+const userModel= require('./models/user');
+
+const { requireAuth } = require('./models/auth');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3002;
+
+// console.log(process.env);
 
 app
+    // .use('/', express.static('server/public/'))
+    .use('/', express.static(__dirname +'/public/'))
+
+    .use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+      next();
+    })
+    
     .use(express.json())
 
-    .get('/api/', (req, res) => {
-        res.send('You are on the homepage');
+    .use((req, res, next) => {
+        const auth = req.headers.authorization;
+        if(auth){
+            const token = auth.split(' ')[1];
+            userModel.fromToken(token)
+                .then(user => {
+                  req.user = user;
+                  next();
+                }).catch(next);
+        } else {
+            next();
+        }
     })
 
+    .get('/api/', (req, res) =>{
+        res.send('You are at the root of the API. For the best class ever ')
+    })
     .use('/api/users', usersController)
+
+    .use((err, req, res, next) => {
+      console.error(err);
+      
+      res .status(err.statusCode || 500)
+            .send({ errors: [ err.message ?? 'Internal server error' ] });
+    })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
