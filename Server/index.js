e4f7a682683d00daab1858dbd632bed3fad55079
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-04-28 20:09:33 -0400

    fixed CORS error

--- Server/index.js
+++ Server/index.js
@@ -19,6 +19,11 @@ app
     .use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      if(req.method === 'OPTIONS') {
+        res.send("OK");
+        return;
+      }
       next();
     })
     
