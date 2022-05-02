ca8437d33bf737053523ce64a9751c044bd4fd93
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-05-02 14:27:13 -0400

    Modified API url

--- Client/src/models/myFetch.ts
+++ Client/src/models/myFetch.ts
@@ -1,7 +1,7 @@
 /* B"H
 */
 
-const API_URL = 'http://localhost:3002/api/';
+const API_URL = 'http://localhost:3001/api/';
 
 export function api(url: string, body?: any, method?: string, headers?: HeadersInit) {
     let options: RequestInit = { headers };
