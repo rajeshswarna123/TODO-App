ec0f8493c683e6f9e150a41ab51ffaeb93083b39
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-05-02 22:16:19 -0400

    Added Generic component for Accordion

---
+++ Client/src/components/AppAccordion.vue
@@ -0,0 +1,32 @@
+<template>
+  <tr>
+    <span
+      @click="toggleAccordion()"
+      class="flex items-center space-x-3"
+      :aria-expanded="isOpen"
+      :aria-controls="`collapse${_uid}`"
+    >
+      <slot name="title" />
+    </span>
+
+    <div v-show="isOpen" :id="`collapse${_uid}`" style="max-width: 12rem;">
+      <slot name="content" />
+    </div>
+  </tr>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+
+  methods: {
+    toggleAccordion() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
+</script>
