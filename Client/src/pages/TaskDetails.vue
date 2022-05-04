ef200d7b1fed47a9d8d04c726eb19a87dd063208
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-05-03 21:13:25 -0400

    Task updation and comment task integration

--- Client/src/pages/TaskDetails.vue
+++ Client/src/pages/TaskDetails.vue
@@ -1,5 +1,6 @@
 
 <script setup lang="ts">
+import moment from "moment";
 import { ref } from "vue-demi";
 import { useRoute } from "vue-router";
 import { useSession } from "../models/session";
@@ -13,7 +14,7 @@ import { Task, usetasks } from "../models/task";
 
     tasks.tasks.forEach(t => {
         if (t._id === route.params.id) {
-            t.dueDate = new Date(t.dueDate);
+            t.dueDate = moment(t.dueDate).format('YYYY-MM-DD');
             task = t;
             console.log(task);
         }
@@ -25,10 +26,13 @@ import { Task, usetasks } from "../models/task";
     });
     }
     else
-        userHandles.value = session.userHandles;
+    userHandles.value = session.userHandles;
+    
     
     function update(){
-
+        tasks.updateTask(task._id, task).then(()=>{
+            console.log('updated');
+        });
     }
 
     function addComment(){
@@ -39,29 +43,46 @@ import { Task, usetasks } from "../models/task";
 
 <template>
     <div>
+        <div class="my-3 py-3">
+            <h3>Task Details</h3>
+        </div>
         <form class="form" @submit.prevent="update">
             <div class="field">
                 <div class="control is-medium">
-                    <input class="input is-medium" type="text" v-model="task.title">
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="floatingInputGrid" v-model="task.title">
+                        <label for="floatingInputGrid">Task Title</label>
+                    </div>
                 </div>
             </div>
             <div class="field">
                 <div class="control is-medium">
-                    <input class="input" type="date" v-model="task.dueDate"/>
+                     <div class="form-floating">
+                        <input class="form-control" id="floatingInputGrid"  type="date" v-model="task.dueDate">
+                        <label for="floatingInputGrid">Due Date</label>
+                    </div>
+                    <!-- <input class="input" type="date" v-model="task.dueDate"/> -->
                 </div>
             </div>
             <div class="field">
                 <div class="control is-medium">
-                    <select v-model="task.assignedTo" class="select">
-                        <option :v-for="user in userHandles" :value="user?._id">{{user?.handle}}</option>
-                    </select>
+                    <div class="form-floating">
+                        <select v-model="task.assignedTo" class="form-select"  id="floatingSelectGrid" aria-label="Floating label select example">
+                            <option v-for="user in userHandles" :value="user?._id">{{user?.handle}}</option>
+                        </select>
+                        <label for="floatingSelectGrid">Works with selects</label>
+                    </div>
+                    
                 </div>
             </div>
-            <button class="button is-primary is-light is-outlined">Update</button>
+            <button class="button is-primary is-light is-outlined">Update Task</button>
         </form>
-
+        <hr />
         <form class="form mb-5" @submit.prevent="addComment">
-            <textarea class="input" type="text" placeholder="Comment" v-model="comment"/>
+            <div class="field form-floating">
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px" v-model="comment"></textarea>
+                <label for="floatingTextarea2">Comments</label>
+            </div>
             <button class="button is-primary is-light is-outlined">Add Comment</button>
         </form>
     </div>
