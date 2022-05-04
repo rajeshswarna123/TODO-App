b75e83b65326aa689f8907503209bcf903066feb
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-05-03 22:00:20 -0400

    Added comments service at client side

---
+++ Client/src/models/comment.ts
@@ -0,0 +1,21 @@
+import { useSession } from './session';
+
+const session = useSession();
+
+export async function getComments(taskId: string){
+    return await session.api('comments/task/'+taskId);
+}
+
+export async function addComment(comment: Comment){
+    return await session.api('comments/', {comment: comment});
+}
+
+
+interface Comment {
+    _id?: string;
+    text: string;
+    taskId: string;
+    isReply: boolean;
+    createdDate: string;
+    modifiedDate: string;
+}
