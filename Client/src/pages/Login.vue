838fc137c22c9f0e07a1a9a788a294bf1ffc12d7
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-05-10 00:34:29 -0400

    Added Rakhi login

--- Client/src/pages/Login.vue
+++ Client/src/pages/Login.vue
@@ -6,13 +6,16 @@ const session = useSession();
 
 const username= ref();
 const password= ref();
-function badLogin(){
-    session.Login("Rajesh Sunny", "meme")
+function rakhiLogin(){
+    session.Login("rakeshn1@n.edu", "secret")
 }
 
 function login()
 {
-    session.Login(username.value, password.value)
+    if(username.value && password.value)
+    {
+        session.Login(username.value, password.value)
+    }
 }
 </script>
 
@@ -24,9 +27,11 @@ function login()
                 <div class="form__container">
                 <div class="logo"><img class="logo__pic" src= "../../src/assets/images/fingerprint.svg" width="45"></div>
                 <div class="fieldset">
-                    <div class="field"><input class="input" type="text" placeholder="User Name" required v-model="username"></div>
-                    <div class="field"><input class="input" type="password" placeholder="Password" required v-model="password"></div>
-                </div><button class="btn">Log In</button>
+                    <div class="field"><input class="input" type="text" placeholder="Email" v-model="username"></div>
+                    <div class="field"><input class="input" type="password" placeholder="Password" v-model="password"></div>
+                </div>
+                    <button class="btn">Log In</button>
+                    <button class="btn mt-4" @click="rakhiLogin">Rakhi Login</button>
                 <div class="text">By creating an account, you agree and accept our <a href="#">Terms</a> and <a href="#">Privacy Policy</a>.</div>
                 </div>
                 <div class="form__footer">Don't Have an Account? <router-link to="/signup">Sign Up</router-link></div>
