4f65e289adf637b7c3e34bd46be0a7c48011e5dc
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-05-17 14:12:41 -0400

    Added info icon in home page

--- Client/src/pages/Home.vue
+++ Client/src/pages/Home.vue
@@ -2,6 +2,7 @@
 <script setup>
 import { computed, ref } from 'vue';
 import moment from 'moment';
+import Popper from 'vue3-popper'
 
 import { usetasks } from '../models/task'
 import * as users from "../models/user";
@@ -16,6 +17,7 @@ const newTask=ref();
 const dueDate=ref();
 const assignedTo=ref();
 const isDescending=ref(false);
+const showPopper=ref(false);
 const userHandles = ref([]);
 if(!session.userHandles || (session.userHandles.length===0)){
   session.GetUserHandles().then(()=>{
@@ -51,7 +53,7 @@ function sortBy(prop) {
           <div class="column">
               <article class="panel">
                 <div class="tabs is-boxed">
-                  <ul>
+                  <ul class="p-0">
                     <li class="is-current" :class="{ 'is-active': currentTab == 'Current'}" @click="currentTab='Current'">
                       <a>
                         <span class="icon is-small"><i class="fas fa-clipboard-list" aria-hidden="true"></i></span>
@@ -79,7 +81,10 @@ function sortBy(prop) {
                   <thead>
                     <tr>
                       <th></th>
-                      <th @click="sortBy('message', isDescending)">Title</th>
+                      <th @click="sortBy('message', isDescending)">Title <Popper arrow :show="showPopper"  content="Click on task title for more details">
+    <i class="fa fa-info-circle fa-sm"  @mouseover="showPopper = true"
+    @mouseleave="showPopper = false" style="color:#00000052" aria-hidden="true"></i>
+  </Popper></th>
                       <th @click="sortBy('dueDate', isDescending)">Due date</th>
                       <th>Assigned to</th>
                       <th>Owner</th>
@@ -98,5 +103,21 @@ function sortBy(prop) {
 
 
 <style lang="scss" scoped>
+:deep(.popper) {
+    background: #000000a8;
+    padding: 20px;
+    border-radius: 20px;
+    color: #fff;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
 
+  :deep(.popper #arrow::before) {
+    background: #00000052;
+  }
+
+  :deep(.popper:hover),
+  :deep(.popper:hover > #arrow::before) {
+    background: #00000052;
+  }
 </style>
