c3beafe3c50978e7d55e02811b91879903bb4e62
Rajesh Swarna <rajeshswarna123@gmail.com>
2022-05-19 13:20:08 -0400

    Added sample typehead in main.ts

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -3,10 +3,15 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue'
 import router from './router'
+import SimpleTypeahead from 'vue3-simple-typeahead';
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import 'vue3-simple-typeahead/dist/vue3-simple-typeahead.css'; //Optional default CSS
+
 
 createApp(App)
     .use(router)
     .use(createPinia())
+    .use(SimpleTypeahead)
     .mount('#app')
